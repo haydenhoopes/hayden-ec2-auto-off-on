f2047384b3f8efece6e77affcf1ad13b37c789b5
haydenhoopes <50504383+haydenhoopes@users.noreply.github.com>
2021-11-15 13:03:39 -0700

    made a weird fix since ec2 keeps dropping my instances

--- lambda_function.py
+++ lambda_function.py
@@ -1,35 +1,35 @@
 import boto3
+from json import dumps
 
 ec2 = boto3.client('ec2', region_name="us-east-1")
 
 def lambda_handler(event, context):
+    instance = ec2.describe_instances()['Reservations'][0]['Instances'][0]['InstanceId'] # Only works if there is a single EC2 Instance
+
     if event['action'] == 'start':
         try:
             response = ec2.start_instances(
                 InstanceIds=[
-                    event['instance']
+                    instance
                 ]
             )
             response = response['StartingInstances'][0]
             return {'statusCode': 200, 'message': f'Instance {response["InstanceId"]} is starting and is currently {response["CurrentState"]["Name"]}. Was {response["PreviousState"]["Name"]}.'}
         except Exception as e:
-            return e.message
+            return dumps(e)
 
         
     elif event['action'] == 'stop':
         try:
             response = ec2.stop_instances(
                 InstanceIds=[
-                    event['instance']
+                    instance
                 ]
             )
             response = response['StoppingInstances'][0]
             return {'statusCode': 200, 'message': f'Instance {response["InstanceId"]} is stopping and is currently {response["CurrentState"]["Name"]}. Was {response["PreviousState"]["Name"]}.'}
         except Exception as e:
-            return e
+            return dumps(e)
 
     else:
         return {'statusCode': 401, 'message': 'No action provided'}
-
-a = lambda_handler({"action":"start", "instance":"123"}, 1)
-print(a)
